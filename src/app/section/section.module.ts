0c64f14c2a2f872851827b2b17837240720494fc
EmaJuarezDev <emanuel.juarez1999@outlook.com>
2022-01-14 16:05:19 -0300

    1 file modified

--- src/app/section/section.module.ts
+++ src/app/section/section.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { SorteoComponent } from './sorteo/sorteo.component';
 import { SectionComponent } from './section.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [BrowserModule, CommonModule, FormsModule],
   declarations: [SectionComponent, SorteoComponent],
   exports: [SectionComponent],
 })
